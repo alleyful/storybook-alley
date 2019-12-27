3001135cfc35a6e9fb5b02c2c64914692281eb12
alleyful <alleyful@naver.com>
2019-12-27 23:21:37 +0900

    add style

--- src/Button/Button_ex.tsx
+++ src/Button/Button_ex.tsx
@@ -42,26 +42,118 @@ ButtonEx.defaultProps = {
   size: "medium"
 };
 
-const style = css``;
+const style = css`
+  outline: none;
+  border: none;
+  box-sizing: border-box;
+  height: 2rem;
+  font-size: 0.875rem;
+  padding: 0 1rem;
+  border-radius: 0.25rem;
+  line-height: 1;
+  font-weight: 600;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  &:focus {
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+  }
+  &:disabled {
+    cursor: not-allowed;
+  }
+  svg {
+    width: 1em;
+    margin-right: 1em;
+  }
+`;
 
 const themes = {
-  primary: css``,
-  secondary: css``,
-  tertiary: css``
+  primary: css`
+    background: #20c997;
+    color: white;
+    svg {
+      fill: white;
+    }
+    &:hover {
+      background: #38d9a9;
+    }
+    &:active {
+      background: #12b886;
+    }
+    &:disabled {
+      background: #aed9cc;
+    }
+  `,
+  secondary: css`
+    background: #e9ecef;
+    color: #343a40;
+    svg {
+      fill: #343a40;
+    }
+    &:hover {
+      background: #f1f3f5;
+    }
+    &:active {
+      background: #dee2e6;
+    }
+    &:disabled {
+      color: #c6d3e1;
+    }
+  `,
+  tertiary: css`
+    background: none;
+    color: #20c997;
+    svg {
+      fill: #20c997;
+    }
+    &:hover {
+      background: #e6fcf5;
+    }
+    &:active {
+      background: #c3fae8;
+    }
+    &:disabled {
+      color: #bcd9d0;
+    }
+  `
 };
 
 const sizes = {
-  small: css``,
-  medium: css``,
-  big: css``
+  small: css`
+    height: 1.75rem;
+    font-size: 0.75rem;
+    padding: 0 0.875rem;
+  `,
+  medium: css`
+    height: 2.5rem;
+    font-size: 1rem;
+    padding: 0 1rem;
+  `,
+  big: css`
+    height: 3rem;
+    font-size: 1.125rem;
+    padding: 0 1.5rem;
+  `
 };
 
-const iconOnlyStyle = css``;
+const iconOnlyStyle = css`
+  padding: 0;
+  border-radius: 50%;
+  svg {
+    margin: 0;
+  }
+`;
 
 const iconOnlySizes = {
-  small: css``,
-  medium: css``,
-  big: ``
+  small: css`
+    width: 1.75rem;
+  `,
+  medium: css`
+    width: 2.5rem;
+  `,
+  big: css`
+    width: 3rem;
+  `
 };
 
 export default ButtonEx;
