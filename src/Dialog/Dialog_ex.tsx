71f32ba5497f5fa7ee0b830ef5228f1493ce61d2
alleyful <alleyful@naver.com>
2019-12-31 18:15:09 +0900

    dialog ex #2

--- src/Dialog/Dialog_ex.tsx
+++ src/Dialog/Dialog_ex.tsx
@@ -1,8 +1,71 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { Fragment } from "react";
+import { css, jsx } from "@emotion/core";
+import ButtonGroup from "../ButtonGroup/ButtonGroup";
+import Button from "../Button/Button";
 
-export const DialogExProps = {};
+export type DialogExProps = {
+  visible: boolean;
+  title?: string;
+  description?: string;
+  children?: React.ReactNode;
+  hideButtons?: boolean;
+  cancellabel?: boolean;
+  cancelText: string;
+  confirmText: string;
+  onCancel?: () => void;
+  onConfirm?: () => void;
+};
 
-const DialogEx = () => {};
+const DialogEx = ({
+  visible,
+  title,
+  description,
+  children,
+  hideButtons,
+  cancellabel,
+  cancelText,
+  confirmText,
+  onCancel,
+  onConfirm
+}: DialogExProps) => {
+  if (!visible) return null;
+
+  return (
+    <Fragment>
+      <div css={[fullscreen, darkLayer]}></div>
+      <div css={[fullscreen, whiteBoxWrapper]}>
+        <div css={[whiteBox]}>
+          {title && <h3>{title}</h3>}
+          {description && <p>{description}</p>}
+          {children}
+          {!hideButtons && (
+            <ButtonGroup>
+              {cancellabel && (
+                <Button theme="tertiary" onClick={onCancel}>
+                  {cancelText}
+                </Button>
+              )}
+              <Button onClick={onConfirm}>{confirmText}</Button>
+            </ButtonGroup>
+          )}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+DialogEx.defaultProps = {
+  cancelText: "취소",
+  confirmText: "확인"
+};
+
+const fullscreen = css``;
+
+const darkLayer = css``;
+
+const whiteBoxWrapper = css``;
+
+const whiteBox = css``;
 
 export default DialogEx;
