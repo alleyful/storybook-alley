086f69792c0fa63fc43b7899019f786a2deba91f
alleyful <alleyful@naver.com>
2020-01-01 13:17:49 +0900

    dialog ex #3

--- src/Dialog/Dialog_ex.tsx
+++ src/Dialog/Dialog_ex.tsx
@@ -60,12 +60,46 @@ DialogEx.defaultProps = {
   confirmText: "확인"
 };
 
-const fullscreen = css``;
+const fullscreen = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
 
-const darkLayer = css``;
+const darkLayer = css`
+  z-index: 10;
+  background: rgba(0, 0, 0, 0.5);
+`;
 
-const whiteBoxWrapper = css``;
+const whiteBoxWrapper = css`
+  z-index: 15;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const whiteBox = css``;
+const whiteBox = css`
+  box-sizing: border-box;
+  border-radius: 4px;
+  width: 25rem;
+  background: #fff;
+  box-shadow: 0px 4px 8px 8px rgba(0, 0, 0, 0.05);
+  padding: 2rem;
+
+  h3 {
+    font-size: 1.5rem;
+    color: #343a40;
+    margin-top: 0;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.125rem;
+    margin: 0;
+    color: #868e96;
+  }
+`;
 
 export default DialogEx;
